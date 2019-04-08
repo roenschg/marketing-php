ceed339a7c19ecb2b3449d1c1179eaacc465ae9d
roenschg <gerd.roensch@digistore24.team>
2019-04-08 16:29:48 +0200

    Add custom fields api

---
+++ src/CustomFields.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Maropost\Api;
+
+use Maropost\Api\Abstractions\OperationResult;
+use Maropost\Api\Abstractions\Api;
+use Maropost\Api\ResultTypes\GetResult;
+
+/**
+ * Class Contacts
+ * @package Maropost\Api
+ */
+class CustomFields
+{
+    use Api;
+
+    /**
+     * Contacts constructor.
+     * @param $accountId
+     * @param $authToken
+     */
+    public function __construct($accountId, $authToken)
+    {
+        $this->auth_token = $authToken;
+        $this->accountId = $accountId;
+        $this->resource = 'custom_fields';
+    }
+
+    /**
+     * Searches a contact with email and get all the details of the contact
+     *
+     * @param string $email Email for which to get the contact
+     * @return OperationResult
+     */
+    public function get(): OperationResult
+    {
+        return $this->_get(null);
+    }
+}
